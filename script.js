9e4b1365b29c50cccfd341cb85b36621986a572d
David Falson <david@falson.net>
2023-03-16 17:34:26 -0700

    copy over style.css and add async to event listener callback functions

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-$(document).ready(() => {
-    
-})
+$(document).ready(async () => {
+    async function getDeckId() {
+        const resp = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        return resp.data.deck_id
+    }   
+
+
+    async function getCardFromDeck(deckId) {
+        const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        return resp.data.cards[0].image
+    }
+
+    let cardContainer = $('#card-holder');
+    let deckId = await getDeckId();
+
+    $('#draw-card-btn').on('click', async () => {
+            let card = await getCardFromDeck(deckId);          
+            
+            cardImg = $("<img>").attr("src", card)
+
+            // Create a new card element and append it to the card container
+            let cardElement = $("<div class='card'>").append(cardImg);
+            let randomRotation = Math.floor(Math.random() * 360);
+            cardElement.css("transform", `rotate(${randomRotation}deg)`);
+
+            // Append the card element to the card container
+            cardContainer.append(cardElement);
+            })
+
+
+
+
+});
